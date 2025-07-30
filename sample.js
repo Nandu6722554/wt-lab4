bcb6e8dfb6fb07c1d04a900a4acac535bd7faa1b
Nandha <kukunoornandu@gmail.com>
2025-07-30 12:06:04 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,5 @@
+alert("Using Javascript")
+document.writeln("Sample Page Admin")
+prompt("Enter into the Login Page!","Enter 1")
+let a=100;
+
---
+++ vardata.js
@@ -0,0 +1,18 @@
+let length = 16;
+let weight = 7.5;
+let color = "Yellow";
+let lastName = "Johnson";
+let x = true;
+let y = false;
+const person = {firstName:"John", lastName:"Doe"};
+const cars = ["Saab", "Volvo", "BMW"];
+const date = new Date("2022-03-25");
+console.log("length:", length);
+console.log("weight:", weight);
+console.log("color:", color);
+console.log("lastName:", lastName);
+console.log("x:", x);
+console.log("y:", y);
+console.log("person:", person);
+console.log("cars:", cars);
+console.log("date:", date);
